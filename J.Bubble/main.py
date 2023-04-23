f4791331c21bdeff65bb60176d0e8b2b353b384b
Alexander <piachev.a@ya.ru>
2023-04-23 17:30:10 +1000

    Add  task J.Bubble pass the contest

--- J.Bubble/main.py
+++ J.Bubble/main.py
@@ -2,33 +2,34 @@ from typing import List, Tuple
 
 
 def load_data() -> Tuple[int, List[int]]:
-    file = open('./J.Bubble/input.txt', 'rt')
+    file = open('./input.txt', 'rt')
     length = int(file.readline())
     data = [int(x) for x in file.readline().split()]
     return length, data
 
 
 def bubble_sort(length: int, digits: List[int]) -> None:
-    one_iter: int = length - 2
     index: int = length - 1
     idx: int = length - 1
-    check: List[bool] = [False for x in range(length-1)]
+
     while index != 0:
         inner = 0
+        check: int = 0
         while inner < idx:
             if digits[inner] > digits[inner+1]:
-                # tmp = digits[inner+1]
-                # digits[inner+1] = digits[inner]
-                # digits[inner] = tmp
                 digits[inner+1], digits[inner] = digits[inner], digits[inner+1]
-                check[inner] = True
+                check += 1
+
             inner += 1
             if inner == idx:
+                if check == 0:
+                    if index == length - 1:
+                        print(' '.join(map(str, digits)))
+                    return
                 idx -= 1
                 index -= 1
+
                 print(' '.join(map(str, digits)))
-        if index == one_iter:
-            index = sum(check) - 1
 
 
 if __name__ == '__main__':
