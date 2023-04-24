25e871f3215be8b0c4ad37692336369fd5849f4f
Alex Ter <urbanmen93@gmail.com>
2023-04-24 13:49:25 +1000

    Solve LargeNumber

--- H.LargeNumber/main.py
+++ H.LargeNumber/main.py
@@ -2,53 +2,16 @@ from typing import List, Tuple, Callable
 
 
 def load_data() -> Tuple[int, List[int]]:
-    file = open('./input.txt', 'rt')
+    file = open('./H.LargeNumber/input.txt', 'rt')
     length = int(file.readline())
     data = [int(x) for x in file.readline().split()]
     return length, data
 
 
-def get_number_digit(number: int) -> int:
-    prev = number
-    index = 0
-    while number > 0:
-        prev = number
-        number = number // 10
-        index += 1
-    return index
-
-
-def get_last_digit(number: int) -> int:
-    prev = number
-    while number > 0:
-        prev = number
-        number = number // 10
-    return prev
-
-
-def summa(num: int) -> float:
-    x = get_number_digit(num)
-    return sum(list(map(int, str(num)))) / x
-
-
 def compare(num1: int, num2: int) -> bool:
-    sum1 = summa(num1)
-    sum2 = summa(num2)
-
-    s1 = get_last_digit(num1)
-    s2 = get_last_digit(num2)
-
-    i1 = get_number_digit(num1)
-    i2 = get_number_digit(num2)
-
-    if s1 == s2 and i1 == i2:
-        if sum1 == sum2:
-            return num1 > num2
-        return sum1 > sum2
-    elif i1 != i2:
-        return s1 > s2
-
-    return s1 > s2
+    if int(str(num1) + str(num2)) > int(str(num2) + str(num1)):
+        return True
+    return False
 
 
 def sort_by_compare(size: int, arr: List[int], func: Callable) -> List[int]:
@@ -73,4 +36,3 @@ if __name__ == '__main__':
     size, arr = load_data()
     result = sort_by_compare(size, arr, compare)
     print(''.join(map(str, result)))
-    print(result)
