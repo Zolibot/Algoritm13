cd0ba4348108f7d796f839e92fa5e844f40af0e4
Alex Ter <urbanmen93@gmail.com>
2023-04-24 14:33:46 +1000

    Solve 2 pass SubSequence

--- C.SubSequence/main.py
+++ C.SubSequence/main.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Callable
+from typing import List, Tuple
 
 
 def load_data() -> Tuple[int, List[int]]:
@@ -8,23 +8,6 @@ def load_data() -> Tuple[int, List[int]]:
     return s1, s2
 
 
-def compare(num1: int, num2: int) -> bool:
-    if int(str(num1) + str(num2)) > int(str(num2) + str(num1)):
-        return True
-    return False
-
-
-def sort_by_compare(size: int, arr: List[int], func: Callable) -> List[int]:
-    for i in range(1, size):
-        item_to_insert = arr[i]
-        j = i
-        while j > 0 and func(item_to_insert, arr[j-1]):
-            arr[j] = arr[j-1]
-            j -= 1
-        arr[j] = item_to_insert
-    return arr
-
-
 def sub_sequence(s1: str, s2: str) -> bool:
     size1 = len(s1)
     count = 0
@@ -32,10 +15,9 @@ def sub_sequence(s1: str, s2: str) -> bool:
     for x in s2:
         if s1[s1_index] == x:
             count += 1
-        s1_index += 1
+            s1_index += 1
         if count == size1-1:
             return True
-        
     return False
 
 
