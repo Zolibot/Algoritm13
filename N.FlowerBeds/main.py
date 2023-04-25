7ee7af5aaf6d229c7da9f9287981adecd551426a
Alexander <piachev.a@ya.ru>
2023-04-25 18:03:20 +1000

    Pass all test N.FlowerBeds

--- N.FlowerBeds/main.py
+++ N.FlowerBeds/main.py
@@ -2,31 +2,54 @@ from typing import Tuple, List
 
 
 def load_data() -> Tuple[int, List[List[int]]]:
-    file = open('./N.FlowerBeds/input.txt', 'rt')
+    file = open('./input.txt', 'rt')
     length: int = int(file.readline())
     double_arr: List[List[int]] = [
-        list(map(int, file.readline().split())) for x in range(length)
+        list(map(int, file.readline().split())) for _ in range(length)
     ]
     return length, double_arr
 
 
-def sort_flower_bets():
+def is_between(number, arr: List[int]) -> bool:
+    return arr[0] <= number <= arr[1]
 
-    pass
 
-
-def is_in_seq(arr1: List[int], arr2: List[int]) -> List[int]:
-    if arr2[0] <= arr1[0] <= arr2[1]:
-        arr1[0] = arr2[0]
-    if arr2[0] <= arr1[1] <= arr2[1]:
-        arr1[1] = arr2[1]
+def get_merge_bets(arr1: List[int], arr2: List[int]) -> List[int]:
+    if is_merge_bets(arr1, arr2):
+        if is_between(arr1[0], arr2):
+            arr1[0] = arr2[0]
+        if is_between(arr1[1], arr2):
+            arr1[1] = arr2[1]
     return arr1
+
+
+def is_merge_bets(arr1: List[int], arr2: List[int]) -> bool:
+    if is_between(arr1[0], arr2):
+        if is_between(arr1[1], arr2):
+            return True
+        return True
+    if is_between(arr2[0], arr1):
+        if is_between(arr2[1], arr1):
+            return True
+        return True
+    return False
+
+
+def sort_flower_bets(length: int, arr: List[List[int]]) -> List[List[int]]:
+    result: List[List[int]] = [arr[0]]
+    count = 0
+    for idx in range(length - 1):
+        if not is_merge_bets(result[count], arr[idx + 1]):
+            result.append(arr[idx + 1])
+            count += 1
+        else:
+            result[count] = get_merge_bets(result[count], arr[idx + 1])
+    return result
 
 
 if __name__ == '__main__':
     size, data = load_data()
-    print(size)
-    print(data)
-
-    print(is_in_seq([1, 3], [3, 5]))
-    print(is_in_seq([-1, 4], [5, 6]))
+    data.sort()
+    res = sort_flower_bets(size, data)
+    for x, y in res:
+        print(x, y)
