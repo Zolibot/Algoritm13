fc6b30033cb54cf5cefc5f07b62751124d9e96df
Alex Ter <urbanmen132@yandex.ru>
2023-04-28 13:41:04 +1000

    Interem changes

--- FinalTask/B.EfficientQuicksort/test.py
+++ FinalTask/B.EfficientQuicksort/test.py
@@ -1,9 +1,12 @@
+""" Doc string. """
 from random import choice
+from typing import List
 
-count = 0
+COUNT = 0
 
 
 def partition(array, pivot):
+    """ Doc string. """
     left = [x for x in array if x < pivot]
     center = [x for x in array if x == pivot]
     right = [x for x in array if x > pivot]
@@ -11,8 +14,9 @@ def partition(array, pivot):
 
 
 def quicksort(array):
-    global count
-    count += 1
+    """ Doc string. """
+    global COUNT
+    COUNT += 1
     if len(array) < 2:
         return array
     else:
@@ -21,7 +25,59 @@ def quicksort(array):
         return quicksort(left) + center + quicksort(right)
 
 
-if __name__ == '__main__':
-    arr = [9, 7, 8, 6, 5, 4, 3, 2, 1, 0]
-    print(quicksort(arr))
-    print(count)
+def swap(array: List[int], first: int, second: int) -> None:
+    """ Doc string. """
+    array[first], array[second] = array[second], array[first]
+
+
+def partition_in_place(array: List[int], pivot: int):
+    """ Doc string. """
+    left = 0
+    right = len(array) - 1
+
+    while True:
+        if array[left] >= pivot >= array[right]:
+            swap(array, left, right)
+            left += 1
+            right -= 1
+
+        if array[left] < pivot :
+            left += 1
+
+        if array[right] > pivot :
+            right -= 1
+
+        if right - left == -1:
+            break
+
+    return array
+
+
+def quicksort_in_place(array: List[int]):
+    """ Doc string. """
+    global COUNT
+    COUNT += 1
+    if len(array) < 2:
+        return array
+    else:
+        pivot = choice(array)
+        partition_in_place(array, pivot)
+        return quicksort(array)
+
+
+if __name__ == "__main__":
+    # arr = [9, 7, 8, 6, 5, 4, 3, 2, 1, 0]
+    # print(quicksort(arr))
+    # print(COUNT)
+    # COUNT = 0
+    
+
+    arr = [4, 8, 9, 20, 1, 5, 3, 10]
+    partition_in_place(arr, 5)
+    print(arr)
+    partition_in_place(arr, 4)
+    print(arr)
+    partition_in_place(arr, 3)
+    print(arr)
+    # print(quicksort_in_place(arr))
+    # print(COUNT)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ![Финальное задание](https://contest.yandex.ru/contest/24735/problems/)
+
+### info
+
+![info](https://stackabuse.com/quicksort-in-python/)
