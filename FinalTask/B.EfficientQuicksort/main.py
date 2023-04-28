7f91054f228eb86d1fbdb6eebd6440c2c06a52eb
Alex Ter <urbanmen132@yandex.ru>
2023-04-29 01:54:37 +1000

    Pass local test task B.EfficientQuicksort

---
+++ FinalTask/B.EfficientQuicksort/main.py
@@ -0,0 +1,57 @@
+from typing import List, Callable
+
+
+def swap(num_array: List[int], first: int, second: int) -> None:
+    num_array[first], num_array[second] = num_array[second], num_array[first]
+
+
+def compare(num_array: List[int], index: int, pivot: List[any]) -> bool:
+    man1: List[any] = array[index]
+    man2: List[any] = pivot
+    if man1[1] > man2[1]:
+        return True
+    elif man1[1] == man2[1] and man1[2] < man2[2]:
+        return True
+    elif man1[1] == man2[1] and man1[2] == man2[2] and man1[0] < man2[0]:
+        return True
+    return False
+
+
+def partition(array: List[int], low: int, high: int, func: Callable) -> int:
+    pivot: int = array[high]
+    great_num: int = low - 1
+    for idx in range(low, high):
+        if func(array, idx, pivot):
+            great_num += 1
+            swap(array, great_num, idx)
+        # if array[idx] <= pivot:
+        #     great_num += 1
+        #     swap(array, great_num, idx)
+    great_num += 1
+    swap(array, great_num, high)
+    return great_num
+
+
+def quicksort(array: List[int], low: int, high: int, func: Callable) -> None:
+    if low < high:
+        top_point: int = partition(array, low, high, func)
+        quicksort(array, low, top_point - 1, func)
+        quicksort(array, top_point + 1, high, func)
+
+
+def load_data() -> List[List[any]]:
+    file = open("FinalTask/B.EfficientQuicksort/input.txt", "rt")
+    size = int(file.readline())
+    return [
+        [int(x) if x.isdigit() else x for x in file.readline().split()]
+        for _ in range(size)
+    ]
+
+
+if __name__ == "__main__":
+    array = load_data()
+    print(array)
+    quicksort(array, 0, len(array) - 1, compare)
+    print(array)
+    for name, _, _ in array:
+        print(name)
--- FinalTask/B.EfficientQuicksort/test.py
+++ FinalTask/B.EfficientQuicksort/test.py
@@ -1,128 +1,30 @@
-""" Doc string. """
-import random
-from random import choice
 from typing import List
 
-COUNT = 0
+
+def swap(num_array: List[int], first: int, second: int) -> None:
+    num_array[first], num_array[second] = num_array[second], num_array[first]
 
 
-def partition(array, pivot):
-    """ Doc string. """
-    left = [x for x in array if x < pivot]
-    center = [x for x in array if x == pivot]
-    right = [x for x in array if x > pivot]
-    return left, center, right
+def partition(array: List[int], low: int, high: int) -> int:
+    pivot: int = array[high]
+    great_num: int = low - 1
+    for idx in range(low, high):
+        if array[idx] <= pivot:
+            great_num += 1
+            swap(array, great_num, idx)
+    great_num += 1
+    swap(array, great_num, high)
+    return great_num
 
 
-def quicksort(array):
-    """ Doc string. """
-    global COUNT
-    COUNT += 1
-    if len(array) < 2:
-        return array
-    else:
-        pivot = choice(array)
-        left, center, right = partition(array, pivot)
-        return quicksort(left) + center + quicksort(right)
+def quicksort(array: List[int], low: int, high: int) -> None:
+    if low < high:
+        top_point: int = partition(array, low, high)
+        quicksort(array, low, top_point - 1)
+        quicksort(array, top_point + 1, high)
 
 
-def swap(array: List[int], first: int, second: int) -> None:
-    """ Doc string. """
-    array[first], array[second] = array[second], array[first]
-
-
-def partition_in_place(array: List[int], pivot: int, start: int, end: int):
-    """ Doc string. """
-    left = start
-    right = end - 1
-    print(pivot)
-
-    while True:
-        if array[left] >= pivot >= array[right]:
-            swap(array, left, right)
-            left += 1
-            right -= 1
-
-        if array[left] <= pivot:
-            left += 1
-
-        if array[right] > pivot:
-            right -= 1
-
-        if right - left <= 0:
-
-            break
-
-    return array
-
-
-def quicksort_in_place(array: List[int], start: int, end: int):
-    """ Doc string. """
-    global COUNT
-    COUNT += 1
-    if end - start < 2:
-        return array
-    else:
-        pivot = choice(array)
-        partition_in_place(array, pivot, start, end-1)
-        partition_in_place(array, pivot, start+1, end)
-        return quicksort_in_place(array, start, end)
-
-
-def sub_partition(array, start, end, idx_pivot):
-
-    'returns the position where the pivot winds up'
+if __name__ == '__main__':
+    array = [10, -1, 8, 9, 1, 5]
+    quicksort(array, 0, len(array) - 1)
     print(array)
-    print(array[idx_pivot], idx_pivot)
-    if not (start <= idx_pivot <= end):
-        raise ValueError('idx pivot must be between start and end')
-
-    array[start], array[idx_pivot] = array[idx_pivot], array[start]
-    pivot = array[start]
-    i = start + 1
-    j = start + 1
-
-    while j <= end:
-        if array[j] <= pivot:
-            array[j], array[i] = array[i], array[j]
-            i += 1
-        j += 1
-
-    array[start], array[i - 1] = array[i - 1], array[start]
-    return i - 1
-
-
-def quicksort2(array, start=0, end=None):
-    global COUNT
-    COUNT += 1
-    if end is None:
-        end = len(array) - 1
-
-    if end - start < 1:
-        return
-
-    idx_pivot = random.randint(start, end)
-    i = sub_partition(array, start, end, idx_pivot)
-    #print array, i, idx_pivot
-    quicksort2(array, start, i - 1)
-    quicksort2(array, i + 1, end)
-
-
-if __name__ == "__main__":
-    # arr = [9, 7, 8, 6, 5, 4, 3, 2, 1, 0]
-    # print(quicksort(arr))
-    # print(COUNT)
-    # COUNT = 0
-
-    arr = [4, 8, 9, 9, 9, 9, 20, 1, 5, 3, 10]
-    print(arr)
-    quicksort2(arr)
-    print(arr)
-    print(COUNT)
-
-    arr = ['alla',
-           'gena',
-           'gosha',
-           'rita',
-           'timofey',
-    ]
