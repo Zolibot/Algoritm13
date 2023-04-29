f662879b5fc9fd28035bbdb8d7efb9a801af14aa
Alex Ter <urbanmen132@yandex.ru>
2023-04-29 15:21:03 +1000

    Fix by reviewer 1

--- FinalTask/B.EfficientQuicksort/main.py
+++ FinalTask/B.EfficientQuicksort/main.py
@@ -1,28 +1,51 @@
 # 86638599
-from typing import List, Callable
+from functools import total_ordering
+from typing import Callable, List
+
+
+@total_ordering
+class Member(object):
+
+    def __init__(self, name: str, task: str, score: str) -> None:
+        self.name: str = name
+        self.task: int = int(task)
+        self.score: int = int(score)
+
+    def __eq__(self, other) -> bool:
+        if self.name == other.name:
+            if self.task == other.task:
+                if self.score == other.score:
+                    return True
+        return False
+
+    def __ge__(self, other):
+        if self.task > other.task:
+            return True
+        if self.task == other.task:
+            if self.score < other.score:
+                return True
+            if self.score == other.score:
+                if self.name < other.name:
+                    return True
+        return False
+
+    def __str__(self) -> str:
+        return self.name
 
 
 def swap(num_array: List[int], first: int, second: int) -> None:
     num_array[first], num_array[second] = num_array[second], num_array[first]
 
 
-def compare(num_array: List[int], index: int, pivot: List[any]) -> bool:
-    man1: List[any] = array[index]
-    man2: List[any] = pivot
-    if man1[1] > man2[1]:
-        return True
-    elif man1[1] == man2[1] and man1[2] < man2[2]:
-        return True
-    elif man1[1] == man2[1] and man1[2] == man2[2] and man1[0] < man2[0]:
-        return True
-    return False
+def compare(competitor_1: Member, competitor_2: Member) -> bool:
+    return competitor_1 > competitor_2
 
 
-def partition(array: List[int], low: int, high: int, func: Callable) -> int:
+def partition(array: List[Member], low: int, high: int, func: Callable) -> int:
     pivot: int = array[high]
     great_num: int = low - 1
     for index in range(low, high):
-        if func(array, index, pivot):
+        if func(array[index], pivot):
             great_num += 1
             swap(array, great_num, index)
     great_num += 1
@@ -38,16 +61,25 @@ def quicksort(array: List[int], low: int, high: int, func: Callable) -> None:
 
 
 def load_data() -> List[List[any]]:
-    file = open('./input.txt', 'rt')
+    file = open('FinalTask/B.EfficientQuicksort/input.txt', 'rt')
     size: int = int(file.readline())
-    return [
-        [int(x) if x.isdigit() else x for x in file.readline().split()]
-        for _ in range(size)
-    ]
+    members: List[Member] = []
+    for _ in range(size):
+        members.append(Member(*file.readline().split()))
+    return members
 
 
 if __name__ == '__main__':
     array = load_data()
     quicksort(array, 0, len(array) - 1, compare)
-    for name, _, _ in array:
+    for name in array:
         print(name)
+    man1 = Member('saha', '5', '5000')
+    man2 = Member('saha', '5', '5000')
+    assert (man1 == man2) == True
+    man1 = Member('saha', '6', '5000')
+    man2 = Member('saha', '5', '5000')
+    assert (man1 > man2) == True
+    man1 = Member('saha', '5', '5000')
+    man2 = Member('saha', '5', '2000')
+    assert (man1 < man2) == True
