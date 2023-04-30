ae50405d217781ec16137ca9a248991c395fcafa
Alex Ter <urbanmen132@yandex.ru>
2023-05-01 02:43:04 +1000

    Fix  by review 2

--- FinalTask/B.EfficientQuicksort/main.py
+++ FinalTask/B.EfficientQuicksort/main.py
@@ -1,76 +1,50 @@
-# 86681352
-from functools import total_ordering
-from typing import Callable, List, Tuple
+# 86758263
+from collections import namedtuple
+from random import randrange
+from typing import List
+
+Info = namedtuple('Competitor', 'task fine name')
 
 
-@total_ordering
-class Member(object):
+class Member:
 
     def __init__(self, name_member: str, task: str, fine: str) -> None:
-        self.name: str = name_member
-        self.task: int = int(task)
-        self.fine: int = int(fine)
-
-    def __eq__(self, other) -> bool:
-        if self.name == other.name:
-            if self.task == other.task:
-                if self.fine == other.fine:
-                    return True
-        return False
-
-    def __ge__(self, other):
-        if self.task > other.task:
-            return True
-        if self.task == other.task:
-            if self.fine < other.fine:
-                return True
-            if self.fine == other.fine:
-                if self.name < other.name:
-                    return True
-        return False
+        self.data = Info(-int(task), int(fine),  name_member)
 
     def __str__(self) -> str:
-        return self.name
+        return self.data.name
 
 
-def swap(array: List[Member], first: int, second: int) -> None:
-    array[first], array[second] = array[second], array[first]
-
-
-def compare(competitor_1: Member, competitor_2: Member) -> bool:
-    return competitor_1 > competitor_2
-
-
-def partition(array: List[Member], low: int, high: int, func: Callable) -> int:
-    # noinspection PyTypeChecker
-    pivot: int = array[high]
+def _partition(array: List[Member], low: int, high: int) -> int:
+    rand_pivot: int = randrange(low, high)
+    array[rand_pivot], array[high] = array[high], array[rand_pivot]
+    pivot: int = high
     great_num: int = low - 1
     for index in range(low, high):
-        if func(array[index], pivot):
+        if array[index].data < array[pivot].data:
             great_num += 1
-            swap(array, great_num, index)
+            array[great_num], array[index] = array[index], array[great_num]
     great_num += 1
-    swap(array, great_num, high)
+    array[great_num], array[high] = array[high], array[great_num]
     return great_num
 
 
-def quicksort(array: List[Member], low: int, high: int, func: Callable) -> None:
+def quicksort(array: List[Member], low=0, high=None) -> None:
+    if high is None:
+        high = len(array) - 1
     if low < high:
-        top_point: int = partition(array, low, high, func)
-        quicksort(array, low, top_point - 1, func)
-        quicksort(array, top_point + 1, high, func)
+        top_point: int = _partition(array, low, high)
+        quicksort(array, low, top_point - 1)
+        quicksort(array, top_point + 1, high)
 
 
-def load_data() -> Tuple[List[Member], int]:
+def load_data() -> List[Member]:
     size: int = int(input())
-    members: List[Member] = []
-    for _ in range(size):
-        members.append(Member(*input().split()))
-    return members, size
+    members = [Member(*input().split()) for _ in range(size)]
+    return members
 
 
 if __name__ == '__main__':
-    competitors, length = load_data()
-    quicksort(competitors, 0, length - 1, compare)
-    for name in competitors:
-        print(name)
+    competitors = load_data()
+    quicksort(competitors)
+    print(*competitors, sep='\n')
